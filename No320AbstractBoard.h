7af0ac0dc7caa8860325ba4a706645ffccdfbabb
shiren1118 <shiren1118@126.com>
2013-05-08 07:50:28 +0800

    t

---
+++ No320AbstractBoard.h
@@ -0,0 +1,13 @@
+//
+//  No320AbstractBoard.h
+//  sinafinance
+//
+//  Created by sang alfred on 5/8/13.
+//
+//
+
+#import "Bee_UIFlowBoard.h"
+
+@interface No320AbstractBoard : BeeUIFlowBoard
+
+@end
